d1f5387edc569359b7598e33495d616ce6f93999
dasha <jjg-lessons@gmail.com>
2024-02-14 20:22:10 +0300

    Занятие №5. Массивы. Строки

---
+++ src/lesson05/ArraysLesson.java
@@ -0,0 +1,109 @@
+package lesson05;
+
+import java.util.Arrays;
+
+public class ArraysLesson {
+    public static void main(String[] args) {
+
+        // массив на 5 элементов,
+        // заполненный дефолтными значениями
+        double[] prices = new double[5];
+        // 5 - длина массива
+        // [0.0, 0.0, 0.0, 0.0, 0.0]
+        // целые числа - 0
+        // числа с плавающей точкой - 0.0
+        // логический тип - false
+        // ссылочные типы - null
+
+        // массив на 6 элементов,
+        // заполненный пользовательскими значениями
+        int[] temps = {45, 90, 122, 33, -3, 67};
+        System.out.println(temps[2]); // 122
+        prices[1] = 10.6; // [0.0, 10.6, 0.0, 0.0, 0.0]
+
+        System.out.println(temps);
+        // import java.util.Arrays;
+        System.out.println(Arrays.toString(temps));
+        System.out.println(temps.length); // 6
+
+        System.out.println(temps[temps.length-1]);
+
+        // java.lang.ArrayIndexOutOfBoundsException
+        // System.out.println(temps[10]);
+
+        // fori
+        // for
+        for (int index = 0; index < prices.length; index++) {
+            System.out.println(prices[index]);
+            prices[index] += 10;
+        }
+        // int[] temps = {45, 90, 122, 33, -3, 67};
+        // перебрать массив temps, температуры ниже 0
+        // заменить на 0,
+        // двигаться от последнего элемента к первому
+
+        for (int index = temps.length - 1; index >= 0; index--) {
+            if (temps[index] < 0) temps[index] = 0;
+        }
+
+        // for (типДанных имяПеременной: массив) {
+        //
+        // }
+
+        // iter
+        for (int temp : temps) {
+            System.out.println(temp);
+        }
+        for (var price : prices) {
+            if (price > 100) {
+                System.out.println(price);
+                break;
+            }
+        }
+        // найти сумму элементов массива temps
+        // {45, 90, 122, 33, 0, 67}
+        int sum = 0;
+        for (int temp : temps) {
+            sum += temp;
+        }
+        System.out.println(sum);
+
+        // Arrays
+        int[] ids = {3, 6, 8, 10, 22, 45}; // 100
+        System.out.println(Arrays.binarySearch(ids, 8)); // 2
+        System.out.println(Arrays.binarySearch(ids, 100)); // -7
+
+        Arrays.sort(temps);
+        System.out.println(Arrays.toString(temps));
+
+        String[] colors = {"red", "blue", null, "orange"};
+
+        for (String color : colors) {
+            if (color != null) System.out.println(color.toUpperCase());
+        }
+
+        int a = 9;
+        int b = a;
+        // a 90
+        // b 10
+
+        // String[] colors = {"red", "blue", null, "orange"};
+        // String[] newColors = colors;
+        // colors -> {"red", "blue", "null", "green"}
+        // newColors -> {"red", "blue", "null", "green"}
+
+
+        String[] newColors01 = colors.clone();
+        String[] newColors02 = Arrays.copyOf(colors, 2);
+        // // String[] colors = {"red", "blue", null, "orange"};
+        String[] newColors03 = new String[colors.length * 2];
+        // [null, "red", "blue", null,"orange", null]
+        System.arraycopy(colors, 0, newColors03, 1, 3);
+
+
+
+
+
+
+    }
+}
---
+++ src/lesson05/StringsLesson.java
@@ -0,0 +1,181 @@
+package lesson05;
+
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.Formatter;
+import java.util.Locale;
+
+public class StringsLesson {
+    public static void main(String[] args) {
+        // char (примитивный тип данных) 16 бит - символ Unicode
+        char char01 = 'J'; // сам символ в одинарных кавычках
+        // но "J" - строка, ссылочный тип String
+        char char02 = 74; // номер символа в таблице Unicode в диапазоне от 0 до 65535
+        char char03 = '\u0044'; // 16ричное представление в escape последовательности
+
+        System.out.print(char01);
+        System.out.print(char02);
+        System.out.print(char03);
+
+        System.out.println('\uD83D');
+        System.out.println('\uDC3E');
+
+        System.out.println("\uD83D\uDC3E"); // суррогатная пара
+
+        // каждый символ юникода может быть представлен одним char или парой (суррогатная пара) char
+
+        String string01 = "Cat";
+        String string02 = "Cat";
+        String string03 = new String("Cat");
+
+        // Сравнение ссылок
+        System.out.println(string01 == string02); // true
+        System.out.println(string01 == string03); // false
+
+        // сравнение строк
+        string01 = "Строка";
+        string02 = "строка";
+        if (string01.equals(string02)) System.out.println("Строки равны");
+        if (string01.equalsIgnoreCase(string02)) System.out.println("Строки равны без учета регистра");
+
+        if (string01.compareTo(string02) < 0) { // или compareToIgnoreCase - без учета регистра
+            System.out.println(string01 + " меньше " + string02);
+        } else if (string01.compareTo(string02) > 0) { // или compareToIgnoreCase - без учета регистра"
+            System.out.println(string01 + " больше " + string02);
+        } else {
+            System.out.println("Строки равны");
+        }
+
+        // верхний и нижний регистр
+        string01 = " JaVa ";
+        string02 = " deVEloPEr ";
+        String upperString = string01.toUpperCase();
+        String lowerString = string02.toLowerCase();
+        System.out.println(upperString);
+        System.out.println(lowerString);
+
+
+        // удаление пробелов с начала и конца строки
+        // проверка на последовательность символов в начале и конце строки
+        // замена символов
+        // string01.startsWith();
+        // "str".startsWith();
+        // upperString.strip().startsWith()
+        // trim U+0020
+        if (upperString.strip().startsWith("JA") && lowerString.trim().endsWith("er")) {
+            System.out.println(upperString.replace("A", "!") +
+                    lowerString.repeat(2));
+        }
+
+
+        // массивы из строк
+        string01 = "java junior developer";
+        String[] strings = string01.split(" ");
+        System.out.println(Arrays.toString(strings));
+
+        char[] chars = string01.toCharArray();
+        System.out.println(Arrays.toString(chars));
+
+        // длина строки
+        System.out.println(string01.length());
+
+        // конкатенация строк
+        string01 = "Hello";
+        string02 = "World";
+        // оператор +
+        String concat = string01 + " " + string02;
+        concat = "Hello" + " " + "World";
+        // метод concat
+        concat = string01.concat(" ").concat(string02);
+
+        // StringBuilder или StringBuffer (использовать для конкатенации в цикле)
+        StringBuilder sb = new StringBuilder();
+        sb.append(string01).append(" ").append(string02).append("!!!");
+        concat = sb.toString();
+        System.out.println(concat);
+
+        String reversed = sb.reverse()
+                .delete(0, 3)
+                .insert(1, "???")
+                .toString();
+        System.out.println(reversed);
+
+        String[][] matrix01 = new String[3][3];
+        String[][] matrix02 = {
+                {"open", "close", "process"},
+                {"close", "close", "process"},
+                {"process", "close", "close"}
+        };
+
+        String[][] animals = new String[3][]; // [null, null, null]
+        animals[0] = new String[2]; // [[null, null], null, null]
+        animals[1] = new String[3]; // [[null, null], [null, null, null], null]
+        animals[2] = new String[4]; // [[null, null], [null, null, null], [null, null, null, null]]
+
+        // форматирование строк
+        String item = "Книга";
+        int count = 0;
+        double rating = 5.5;
+        String formattedString =
+                MessageFormat.format("Товар: {0}. На складе: {1}шт. Рейтинг: {2}.",
+                        item, count, rating);
+        System.out.println(formattedString);
+
+        MessageFormat messageFormat = new MessageFormat("Т: {0}." +
+                " Кол: {1, choice, 0#товар отсутствует|0<мало|5<много}. Р: {2}.", Locale.UK);
+        formattedString = messageFormat.format(new Object[]{item, count, rating});
+        System.out.println(formattedString);
+
+        // class MessageFormat
+        // {index}
+        // {index, type}
+        // {index, type, style}
+        // number:	integer, currency, percent, custom format - class DecimalFormat
+        // date:	short, medium, long, full, custom format - class SimpleDateFormat
+        // time:	short, medium, long, full, custom format - class SimpleDateFormat
+        // choice:	custom format - class ChoiceFormat
+
+        int number = 1;
+        boolean answer = true;
+        double value = 5.77233;
+        formattedString = String.format("%d) Верный ответ: %B", number, answer);
+        System.out.println(formattedString);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        Formatter formatter = new Formatter(stringBuilder);
+
+        formattedString = formatter.format("%d) Верный ответ: %B", number, answer).toString();
+        System.out.println(formattedString);
+        stringBuilder.setLength(0);
+
+        formattedString = formatter.format("%1$d) Верный ответ: %2$b", number, answer).toString();
+        System.out.println(formattedString);
+        stringBuilder.setLength(0);
+
+        formattedString = String.format("%d) Верный ответ: %B", number, answer);
+        System.out.println(formattedString);
+        stringBuilder.setLength(0);
+
+        formattedString = formatter.format("%1$d) Верный ответ: %2$.1f", number, value).toString();
+        System.out.println(formattedString);
+        stringBuilder.setLength(0);
+
+        // %[argument_index$][flags][width][.precision]conversion
+        // % и conversion являются обязательными
+        // conversion указывает на то, как должен быть отформатирован аргумент.
+        // b или B – для логических значений
+        // s или S — для строк (String)
+        // c или C - для символов
+        // d - для десятичного числа
+        // o - для восьмеричного числа
+        // x или X - для шестнадцатеричного числа
+        // f - для чисел с плавающей точкой
+        // t или T - дата и время
+        // argument_index$ - целое число, аргумент из списка
+        // flags - набор символов, используемых для изменения формата вывода.
+        // width — положительное целое число, минимальное количество символов, которое должно быть записано в вывод.
+        // precision - целое число, используется для ограничения количества символов.
+        // Количество цифр после запятой для float и double
+
+    }
+}
