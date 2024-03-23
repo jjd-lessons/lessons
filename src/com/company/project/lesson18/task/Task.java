c1ccb0413c9768a47ea658951ce28f7b8d08f9ca
dasha <jjg-lessons@gmail.com>
2024-03-23 10:17:00 +0300

    Занятие 18. Lambda

--- src/com/company/project/lesson18/task/Task.java
+++ src/com/company/project/lesson18/task/Task.java
@@ -1,12 +1,17 @@
 package com.company.project.lesson18.task;
 
+import java.time.LocalDateTime;
+
 public class Task {
     private int id;
     private String title;
     private Status status;
+    private LocalDateTime createdAt;
+    private LocalDateTime closeTo;
 
+    // геттеры, сеттеры и д.т
 
     public enum Status{
-
+        NEW, IN_PROGRESS, CLOSED
     }
 }
