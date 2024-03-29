d950fc098695c5647d8ab482dd5c3ca659c18e9f
dasha <jjg-lessons@gmail.com>
2024-03-29 19:17:26 +0300

    Занятие 21. Collectors API

---
+++ src/com/company/project/lesson21/Lesson21.java
@@ -0,0 +1,66 @@
+package com.company.project.lesson21;
+
+import com.company.project.lesson18.Operation;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+class HelpClass {
+    public double minus(double a, double b){
+        return a - b;
+    }
+    public static double sub(double a, double b){
+        return a * b;
+    }
+}
+public class Lesson21 {
+
+    public static void main(String[] args) {
+        Operation plus = (a, b) -> a + b;
+        Operation plusWithRef = Double::sum;
+        Operation sub = (x, y) -> x * y;
+        Operation subRef = HelpClass::sub;
+        Operation minus = (a, b) -> a - b;
+        // ссылка на не static метод предполагает наличие экземпляра
+        // Operation minusRef = HelpClass::minus;
+        Operation minusRef = new HelpClass()::minus;
+
+        List<String> strings = Stream.of("dfqqe", "mfoqgmh", "fhmqu7yf")
+//                .map(string -> string.toUpperCase());
+//                .map(string -> {
+//                   if (string != null) return string.toUpperCase();
+//                   return "EMPTY";
+//                });
+                .map(String::toUpperCase)
+                .toList();
+    }
+
+
+
+
+    public int someVoid01(String param01, String[] param02){
+        Optional<String> stringOptional = Optional.ofNullable(param01);
+        Optional<String[]> stringArrOptional = Optional.ofNullable(param02);
+        if (stringOptional.isPresent() && stringArrOptional.isPresent()){
+            return 1;
+        }
+        return 0;
+    }
+    public int someVoid02(String param01, String[] param02){
+        // Optional.of(param01);
+        Objects.requireNonNull(param01);
+        // Optional.of(param02);
+        Objects.requireNonNull(param02);
+        return 1;
+    }
+
+    public int someVoid03(String param01, String[] param02){
+        Optional<String> s = Optional.of(param01)
+                .filter(param->!param.isEmpty())
+                .map(param -> param.toUpperCase());
+        if (s.isEmpty()) return -1;
+        return 1;
+    }
+}
