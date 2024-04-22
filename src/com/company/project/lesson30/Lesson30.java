1636fcd8c6986b413a13754f81a899fd57acb479
dasha <jjg-lessons@gmail.com>
2024-04-22 18:44:58 +0300

    Занятие 30. Многопоточность (№4)

---
+++ src/com/company/project/lesson30/Lesson30.java
@@ -0,0 +1,28 @@
+package com.company.project.lesson30;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Lesson30 {
+    public static void main(String[] args) {
+        List<String> tokens = new ArrayList<>(
+                Arrays.asList("123", "456", "789"));
+
+        ReentrantLock lock = new ReentrantLock(/*true*/);
+
+        Semaphore semaphore = new Semaphore(tokens.size()/*, true*/); // 3
+
+        // токены нужны только для подключения
+        // токен можно использовать только один раз
+        // использованный токен нужно заменить новым
+
+        new SynchronizedClientService(tokens, lock, semaphore).start();
+        new SynchronizedClientService(tokens, lock, semaphore).start();
+        new SynchronizedClientService(tokens, lock, semaphore).start();
+        new SynchronizedClientService(tokens, lock, semaphore).start();
+        new SynchronizedClientService(tokens, lock, semaphore).start();
+    }
+}
