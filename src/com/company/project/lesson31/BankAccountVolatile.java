54db9921fa84a04dba3c62d1e0d6b8fa5c8526c5
dasha <jjg-lessons@gmail.com>
2024-04-24 22:03:38 +0300

    Занятие 31. Многопоточность (№5)

--- src/com/company/project/lesson31/BankAccountVolatile.java
+++ src/com/company/project/lesson31/BankAccountVolatile.java
@@ -4,9 +4,14 @@ package com.company.project.lesson31;
 public class BankAccountVolatile {
     private volatile int balance;
 
-    public void putMoney(int value) {
-        this.balance += value;
+    public synchronized void putMoney(int value) {
+        this.balance = this.balance + value;
     }
+
+/*    public void putMoney(int value) {
+        // атомарная операция + volatile
+        this.balance = value;
+    }*/
 
     public int getBalance() {
         return balance;
