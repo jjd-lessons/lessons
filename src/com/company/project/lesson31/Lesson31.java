57c41b2a95ceb3bb787b6b30a0e0191372d83171
dasha <jjg-lessons@gmail.com>
2024-04-26 21:02:57 +0300

    CompletableFuture

--- src/com/company/project/lesson31/Lesson31.java
+++ src/com/company/project/lesson31/Lesson31.java
@@ -4,12 +4,48 @@ package com.company.project.lesson31;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Lesson31 {
 
+    public static CompletableFuture<String> run(){
+        return CompletableFuture.supplyAsync(()->{
+            System.out.println("Инструкции задачи");
+            return UUID.randomUUID().toString();
+        });
+    }
+
     public static void main(String[] args) {
+        // CompletableFuture<Void>
+        CompletableFuture<Void> future = CompletableFuture.runAsync(()->{
+            System.out.println("Инструкции задачи");
+        });
+
+        CompletableFuture<String> idGen01 = CompletableFuture.supplyAsync(()->{
+            System.out.println("Инструкции задачи");
+            return UUID.randomUUID().toString();
+        });
+        try {
+            idGen01.get();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+
+        run().thenApply((res) -> {
+            return res + UUID.randomUUID();
+        }).thenAccept(res -> {
+            System.out.println(res);
+        }).exceptionally(exception -> {
+            System.out.println(exception.getMessage());
+            throw new RuntimeException(exception);
+        });
+
 
         BankAccountVolatile accountVolatile = new BankAccountVolatile();
         ArrayList<Thread> threadsVolatile = new ArrayList<>();
